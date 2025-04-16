bfcd0a2bb1b454dd916dc9ea883b844964ed6c13
NoAngel <155915830+NopAngel@users.noreply.github.com>
2025-04-16 14:12:27 -0400

    panic.c - panic.h: Fixed an optimization issue that prevented the panic message from being displayed.
    This occurred because the `k_printf.h` function (this function is responsible for displaying a message on the screen) was not being called.

--- panic/panic.c
+++ panic/panic.c
@@ -1,28 +1,12 @@
-unsigned int k_printf_r(char *message, unsigned int line, unsigned int color)
-{
-	char *vidmem = (char *) 0xb8000;
-	unsigned int i=0;
+/*
+*
+*   panic.c - a generic file for panic
+*
+*  (C) 2025 Angel Nieto/NopAngel <angelnieto1402@gmail.com>
+*
+*           This code is licenced under the APACHE 2.0
+*/
 
-	i=(line*80*2);
-
-	while(*message!=0)
-	{
-		if(*message=='\n') // chec1k for a new line (upgrated)
-		{
-			line++;
-			i=(line*80*2);
-			*message++;
-		} else {
-			vidmem[i]=*message;
-			*message++;
-			i++;
-			vidmem[i]=color;
-			i++;
-		};
-	};
-
-	return(1);
-}
 
 // WARNING: I know this needs fixing, but I'm too lazy. I'll do it later. :)
 
@@ -31,6 +15,6 @@ unsigned int k_printf_r(char *message, unsigned int line, unsigned int color)
 
 void kernelPanic(char * reasons) {
 	asm volatile("cli");
-	k_printf_r(reasons, 0x04, 0);
+	k_printf(reasons, 0x04, 0);
 }
 
--- panic/panic.h
+++ panic/panic.h
@@ -1,7 +1,17 @@
+/*
+*
+*   panic.h - a generic file for panic
+*
+*  (C) 2025 Angel Nieto/NopAngel <angelnieto1402@gmail.com>
+*
+*           This code is licenced under the APACHE 2.0
+*/
+
+
 #ifndef KERNEL_PANIC_H
 #define KERNEL_PANIC_H
 
-unsigned int k_printf_r(char *message, unsigned int line, unsigned int color);
+#include "../fs/k_printf.h"
 unsigned int kernelPanic(const char** reasons);
 
 #endif
