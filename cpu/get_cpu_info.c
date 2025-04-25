72278ac8142eda5b62a0323d0e145c78f785fa0c
NoAngel <155915830+NopAngel@users.noreply.github.com>
2025-04-25 17:11:42 -0400

    get_cpu_info :: commentary's.

--- cpu/get_cpu_info.c
+++ cpu/get_cpu_info.c
@@ -1,3 +1,13 @@
+/*
+*
+*   get_cpu_info.c - obtain cpu info
+*
+*  (C) 2025 Angel Nieto/NopAngel <angelnieto1402@gmail.com>
+*
+*           This code is licenced under the APACHE 2.0
+*/
+
+
 unsigned int get_cpu_cycles() {
     unsigned int low, high;
     __asm__ __volatile__("rdtsc" : "=a" (low), "=d" (high));
--- cpu/get_cpu_info.h
+++ cpu/get_cpu_info.h
@@ -1,3 +1,13 @@
+/*
+*
+*   get_cpu_info.h - obtain cpu info (file .h)
+*
+*  (C) 2025 Angel Nieto/NopAngel <angelnieto1402@gmail.com>
+*
+*           This code is licenced under the APACHE 2.0
+*/
+
+
 #ifndef GET_CPU_INFO_H
 #define GET_CPU_INFO_H
 
